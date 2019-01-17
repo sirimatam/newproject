8ee020162869a2541f8061dec97a96b41415d790
sirimatam <45136358+sirimatam@users.noreply.github.com>
2019-01-17 17:10:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,10 +10,10 @@ prod_type varchar(40) NOT NULL,
 prod_color varchar(40) NOT NULL,
 prod_description varchar(200) NOT NULL,
 prod_price_per_unit int(10) NOT NULL,
-prod_stock int(10) NOT NULL,
-prod_pro_price int(10) NOT NULL,
+prod_stock int NOT NULL,
+prod_pro_price int NOT NULL,
 PRIMARY KEY(prod_id))");
-
+/*
 pg_query($db,"CREATE TABLE createcart (
 cartp_id varchar(10) NOT NULL,
 cus_id varchar(50) NOT NULL,
@@ -44,7 +44,7 @@ cus_id varchar(50) NOT NULL,
 cus_name varchar(40) NOT NULL,
 cus_address varchar(100) NOT NULL,
 cus_tel varchar(10) NOT NULL,
-PRIMARY KEY(prod_id))");
+PRIMARY KEY(cus_id))");
 
 pg_query($db,"CREATE TABLE payment (
 pay_id varchar(10) NOT NULL,
@@ -55,11 +55,12 @@ order_id varchar(10) NOT NULL,
 check varchar(10) NOT NULL,
 FOREIGN KEY (order_id) REFERENCES order(order_id),
 PRIMARY KEY(pay_id))");
-
+*/
+/*
 pg_query($db,"CREATE TABLE staff (
 staff_id varchar(10) NOT NULL,
 staff_status varchar(10) NOT NULL,
 PRIMARY KEY(staff_id))");
 
-
+*/
 ?>
