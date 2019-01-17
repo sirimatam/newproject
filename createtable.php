b5c94f92814a6ee7bc6c8ec8abf86df465a8fe9d
sirimatam <45136358+sirimatam@users.noreply.github.com>
2019-01-17 16:59:20 +0700

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL & ~E_NOTICE);
 
 require_once('connection.php');
 //$db = pg_connect(("host=ec2-107-22-162-8.compute-1.amazonaws.com port=5432 dbname=d8kmurmr59kdsg user=kdozgixaxyediw password=0fad69adabf7a1c52fec6765c9331e776845abe09fc5a3b7c9c5ae1ccc9f6531");
-
+/*
 pg_query($db,"CREATE TABLE product (
 prod_id varchar(10) NOT NULL,
 prod_name varchar(40) NOT NULL,
@@ -68,6 +68,6 @@ PRIMARY KEY(staff_id))");
 
 
 
-
+*/
 
 ?>
