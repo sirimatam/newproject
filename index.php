57159ac102b43462dd904675a3b539dd5203a287
sirimatam <45136358+sirimatam@users.noreply.github.com>
2019-01-20 17:00:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -22,37 +22,55 @@ if ( sizeof($request_array['events']) > 0 )
    {
     $text = $event['message']['text'];
 	
-	$greeting = array('Hi','Hello','ดีจ้า','สวัสดี','สวัสดีครับ');
-	
-	$correct = 0;
-	foreach ($greeting as $value)
-	{
-		if ($text == $value)
-		{
-			$correct = 1;
-		}
-	}
-	if ($correct == 1)
-	{
-		$reply_message = 'Hi,what is you name';
-	}
-	elseif ($text=='button')
+	if ($text=='ดู และ สั่งซื้อสินค้า')
 	{
 		$reply_message = "1";
 	}
 	
-	
+	elseif ($text=='สินค้าโปรโมชั่น')
+	{
+		$reply_message = "2";
+	}
+  elseif ($text=='ตะกร้าสินค้าที่บันทึกไว้')
+	{
+		$reply_message = "3";
+	}
+	elseif ($text=='เพิ่ม/แก้ไขที่อยู่จัดส่ง')
+	{
+		$reply_message = "4";
+	}
+  elseif ($text=='wishlist')
+	{
+		$reply_message = "5";
+	}
+  elseif ($text=='เช็คสถานะจ่ายเงิน/พัสดุ')
+	{
+		$reply_message = "6";
+	}
+/*	elseif (substr($text,0,6) =='addcus')
+	{
+		list($order, $cusid, $cusname, $cuslast, $cuspic) = split(" ", $text, 5);
+		//$cardata = explode(" ",$text);
+		pg_query($db,"INSERT INTO Customer1 (cus_id,cus_name,cus_lastname,cus_pic) VALUES ($cusid,$cusname,$cuslast,$cuspic)");
+		$result = pg_query($db,"SELECT cus_name FROM Customer1");
+		while ($list = pg_fetch_row($result))
+		{
+			$cust = $list[0]."\n";
+			$custlist .= $cust;
+		}
+		$reply_message = "$custlist";
+	}
+	*/
+   }
 	else
 	$reply_message = 'why dont you say hello to me';
    }
    else
     $reply_message = 'ระบบได้รับ '.ucfirst($event['message']['type']).' ของคุณแล้ว';
   
-  }
+  }}
   else
    $reply_message = 'ระบบได้รับ Event '.ucfirst($event['type']).' ของคุณแล้ว';
- }
-	 
 
 
  if( strlen($reply_message) > 0 )
