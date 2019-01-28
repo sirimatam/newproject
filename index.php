d0c3058193b76dfa157df3871952d8d7b9ca0fbf
sirimatam <45136358+sirimatam@users.noreply.github.com>
2019-01-29 01:03:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ if ( sizeof($request_array['events']) > 0 )
    {
         $text = $event['message']['text']; 
 	$userid = $event['source']['userId'];
-	$findid = pg_query($db,"SELECT * FROM customer WHERE cus_id = $userid");
+	$findid = pg_query($db,"SELECT * FROM customer WHERE cus_id = '$userid'");
 	if( sizeof(pg_fetch_row($findid))[0] == 0)
 	{
 		pg_query($db,"INSERT INTO customer (cus_id) VALUES $userid");
