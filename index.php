0b2556684481dc0f159d6fec004afca96e6fc5ce
sirimatam <45136358+sirimatam@users.noreply.github.com>
2019-01-28 20:15:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ if ( sizeof($request_array['events']) > 0 )
 	$findid = pg_query($db,"SELECT * FROM customer WHERE cus_id = $userid");
 	if( sizeof(pg_fetch_row($findid)[0] == 0)
 	{
-		pg_query($db,"INSERT INTO customer (cus_id) VALUES ($userid)");
+		pg_query($db,"INSERT INTO customer (cus_id) VALUES $userid");
 		pg_query($db,"INSERT INTO createcart VALUES (cus_id) VALUES $userid");
 	}
 	
