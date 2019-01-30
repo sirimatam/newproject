d401b8c96f5c95c0f982afb3ccc2b603b442be8a
ssirawan <45140043+ssirawan@users.noreply.github.com>
2019-01-30 10:20:07 +0700

    Update tracking.php

--- tracking.php
+++ tracking.php
@@ -1,9 +1,31 @@
 <?php
 
+// List all supported couriers and corresponding courier code at TrackingMore.
 
 $track = new Trackingmore;
 $track = $track->getCarrierList();
 
+// Detect a carrier by tracking code
+$track = new Trackingmore;
+$trackingNumber = getTracking($orderid); /// function get track
+$track = $track->detectCarrier($trackingNumber);
+
+
+//Create a tracking.
+$track = new Trackingmore;
+$extraInfo                         = array();
+$extraInfo['title']                = 'iphone6';
+$extraInfo['logistics_channel']   = '4PX挂号小包';
+$extraInfo['customer_name']        = 'charse chen';
+$extraInfo['customer_email']       = 'chasechen@gmail.com';
+$extraInfo['order_id']             = '8988787987';
+$extraInfo['customer_phone']       = '86 13873399982';
+$extraInfo['order_create_time']    = '2018-05-11 12:00';
+$extraInfo['destination_code']     = 'US';
+$extraInfo['tracking_ship_date']   = time();
+$extraInfo['tracking_postal_code'] = '13ES20';
+$extraInfo['lang']                 = 'en';
+$track = $track->createTracking('china-post','RM121516216CN',$extraInfo);
 
 
 
@@ -14,11 +36,11 @@ $track = $track->getCarrierList();
 
 
 
-
-
-
-
-
+function getTracking($orderid)
+{
+    $trackingNumber = pg_fetch_row(pg_query($db,"SELECT order_status FROM order WHERE order_id = $orderid"))[0];
+    return $trackingnumber;
+}
 
 
 function track_trace($db,$orderid,$post_header)
