73fdfa9d632574c3e94779ff2625dc27db7806c4
ssirawan <45140043+ssirawan@users.noreply.github.com>
2019-01-30 12:28:26 +0700

    Update tracking.php

--- tracking.php
+++ tracking.php
@@ -30,9 +30,19 @@ $track = $track->createTracking('china-post','RM121516216CN',$extraInfo);
 
 //Get tracking results of a single tracking.
 $track = new Trackingmore;
-$track = $track->getSingleTrackingResult('china-post','RM131516216CN','en');
+$track = $track->getSingleTrackingResult('kerry-logistics',$trackingNumber,'en');
 
 
+//Get realtime tracking results of a single tracking.
+$track = new Trackingmore;
+$extraInfo['destination_code']          = 'US';
+$extraInfo['tracking_ship_date']  = '20180226';
+$extraInfo['tracking_postal_code'] = '13ES20';
+$extraInfo['specialNumberDestination']       = 'US';
+$extraInfo['order']       = '#123123';
+$extraInfo['order_create_time']       = '2017/8/27 16:51';
+$extraInfo['lang']       = 'cn';
+$track = $track->getRealtimeTrackingResults('kerry-logistics',$trackingNumber,$extraInfo);
 
 
 
@@ -45,33 +55,5 @@ function getTracking($orderid)
 }
 
 
-function track_trace($db,$orderid,$post_header)
-{
-    $order_status = pg_fetch_row(pg_query($db,"SELECT order_status FROM order WHERE order_id = $orderid"))[0];
-    $url = 'https://api.trackingmore.com/v2';
-    //https://www.trackingmore.com/api-index.html
-    
-    
-     $apikey = 'c7f2b785-3a87-4a1d-8c10-591095e49b4e';
-    $header = array('Content-Type: application/json', 'Trackingmore-Api-Key: ' .$apikey);
-   
-    
-    $trace = '';
-    
-     $ch = curl_init($url);
-     
-     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-     curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post, JSON_UNESCAPED_UNICODE));
-     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-     $result = curl_exec($ch);
-     curl_close($ch);
-     return $result;
-    
-    
-    
-    
-}
 
 ?>
