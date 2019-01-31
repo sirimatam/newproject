1a578a6c2a7b1d624686d963d9d72680d236e33f
ssirawan <45140043+ssirawan@users.noreply.github.com>
2019-01-31 13:10:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -74,14 +74,11 @@ if ( sizeof($request_array['events']) > 0 )
 		$data['template']['title'] = 'ที่อยู่จัดส่งปัจจุบัน';
 		$data['template']['text'] = 'aaaaa';
 		
-		$ans = [
-		    'replyToken' => $reply_token,
-		    'messages' => [['type' => 'text', 'text' => 'aaa' ]]
-		   ];
+		
 		
 		//$button = show_address($db,$userid);
-		//$post = format_message($reply_token,json_encode($data));
-		send_reply_message($API_URL, $POST_HEADER, $ans);
+		$post = format_message($reply_token,json_encode($data));
+		send_reply_message($API_URL, $POST_HEADER, $data);
 
 	}
 	/*   
