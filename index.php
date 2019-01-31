734ea92e88d9695ef17463c6cd9c5a45966b6051
sirimatam <45136358+sirimatam@users.noreply.github.com>
2019-01-31 17:14:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -77,6 +77,7 @@ $data = [];
 		$post = format_message($reply_token,$array_carousel);	
 	        $send_result = send_reply_message($API_URL, $POST_HEADER, $post);
 		file_put_contents("php://stderr", "POST RESULT =====> ".$send_result);
+		file_put_contents("php://stderr", "POST REQUEST =====> ".$post_body);
 
 		/*
 		if(sizeof($array_carousel) > 1)
