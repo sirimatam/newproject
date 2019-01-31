a35d88f65008b13ad51776b8e3f2e2ee3fb8d819
ssirawan <45140043+ssirawan@users.noreply.github.com>
2019-01-31 12:50:48 +0700

    Update function.php

--- function.php
+++ function.php
@@ -133,10 +133,10 @@ function show_address($db,$cusid)
 {
 	$query = pg_query($db,"SELECT cus_description FROM customer WHERE cus_id = '$cusid' AND cus_default = '1'");
 	$address = pg_fetch_row($query)[0];
-	//$address = 'ddd';
+	/*$address = 'ddd';
 	if ($address === NULL)
 	{ $address = 'กรุณาแก้ไขที่อยู่จัดส่ง';}
-	
+	*/
 	$data = [];
 	$data['type'] = 'template';
 	$data['altText'] = 'this is a buttons template';
