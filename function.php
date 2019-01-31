3faeb1a0bc8e12c4d3c9daff109995ebee37ea40
ssirawan <45140043+ssirawan@users.noreply.github.com>
2019-01-31 14:57:44 +0700

    Update function.php

--- function.php
+++ function.php
@@ -312,7 +312,7 @@ function add_favorite($db,$prod_id,$cus_id)
   
   function carousel_show_favorite($db,$cus_id)
   {
-    $check = pg_query($db,'SELECT * FROM favorite WHERE favorite.cus_id = $cus_id'); 
+    $check = pg_query($db,"SELECT * FROM favorite WHERE favorite.cus_id = '$cus_id'"); 
     $list = pg_fetch_row($check);
     for ($i=0; $i<10;$i++)
      {
