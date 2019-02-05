2f255dfb6b1962cff7297ba95cc3ef9c83ecf54b
ssirawan <45140043+ssirawan@users.noreply.github.com>
2019-02-05 15:44:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -205,9 +205,9 @@ if ( sizeof($request_array['events']) > 0 )
 	   $time = date("H:i:sa");
 	   $date = date("Y/m/d") ;
 	   
-	   pg_guery($db,"INSERT INTO payment VALUES ('1',$img_binary,$date,$time,'order1','0')");
+	   pg_guery($db,"INSERT INTO payment VALUES ('1',$response,$date,$time,'order1','0')");
 	   
-	   $dataa = format_message($reply_token,['type'=>'text','text'=> $response]);
+	   $dataa = format_message($reply_token,['type'=>'text','text'=> 'hello']);
 	   send_reply_message($API_URL, $POST_HEADER, $dataa);
 	   
 	   //$get = get_user_content($GET_url,$POST_HEADER);
