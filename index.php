a46d1ba66ed6be2b7634366b0d1e5da64d2934cd
ssirawan <45140043+ssirawan@users.noreply.github.com>
2019-02-05 23:20:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -204,10 +204,10 @@ if ( sizeof($request_array['events']) > 0 )
 	   $data = base64_decode($img);
 	   
 	   $file = UPLOAD_DIR . $imgid . '.png';
-	   shell_exec('git init');
-	   shell_exec('git add $file');
-	   shell_exec('git commit $file');
-	   shell_exec('git push origin/master');
+	   exec('git init');
+	   exec('git add $file');
+	   exec('git commit $file');
+	   exec('git push origin/master');
 	   
 	   $success = file_put_contents($file, $data);	   
 	   
