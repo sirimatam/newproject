44319e45fd447b7142d5f1ba2fe79ce8a5a20161
ssirawan <45140043+ssirawan@users.noreply.github.com>
2019-02-07 13:36:13 +0700

    Update function.php

--- function.php
+++ function.php
@@ -167,10 +167,8 @@ function show_address($db,$cusid)
 	   $cus_primary = pg_fetch_row($query)[0];
 	   pg_query($db,"UPDATE customer SET cus_default = '1' WHERE cus_primary = '$cus_primary' ");  // in case ลบอันปัจจุบัน อันที่เหลือ =1
 	   $address = pg_fetch_row($query)[2];
-	   if($address != '')
-		 { $address = pg_fetch_row($query)[2];}
-	   else
-	         { $address = 'กรุณาเพิ่ม ชื่อ นามสกุล และที่อยู่จัดส่ง'; }
+	   if(strlen($address) == 0)
+		 { $address = 'กรุณาเพิ่ม ชื่อ นามสกุล และที่อยู่จัดส่ง'; }
 	
 	 }
 	
