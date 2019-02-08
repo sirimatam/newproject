06827eaad44a8d7e62d9662a225cd01c75c58f48
ssirawan <45140043+ssirawan@users.noreply.github.com>
2019-02-08 12:18:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -37,6 +37,7 @@ if ( sizeof($request_array['events']) > 0 )
 	{
 		
 		$data = format_message($reply_token,button_all_type());
+		file_put_contents("php://stderr", "POST RESULT =====>.json_encode($data)");
 		$send_result = send_reply_message($API_URL, $POST_HEADER, $data);
 		
 	}
