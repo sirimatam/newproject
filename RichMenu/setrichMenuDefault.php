a36b66b498fc803a75ca9c4f674a7d93a3475f06
sirimatam <45136358+sirimatam@users.noreply.github.com>
2019-02-13 16:38:20 +0700

    Update setrichMenuDefault.php

--- RichMenu/setrichMenuDefault.php
+++ RichMenu/setrichMenuDefault.php
@@ -4,12 +4,8 @@ $richMenuId1 = "richmenu-ff58dd0a3a6e5f68cfc40afae5abe6ad"; //page1
 $richMenuId2= "richmenu-717a8ebccd0d4a7e0ca2c85d77a50f10"; //page2
 */
 
-
-$ACCESS_TOKEN = 'wa9sF+y4HsXJ2IqRQcTadD32XYH7lG01BLuw9O9AbkTSbdRUvC4CU6vOvAKCE4LGU0AgIBSwSyumjqfA22ZZVWQxrkmbxfDaupCQ3tPD0yrY67su+hl6Iw1oKWVpWo3JWOg7RFFphGSz3x5MY/aqMgdB04t89/1O/w1cDnyilFU=';
-
 //set rich menu default after upload img 
-$ACCESS_TOKEN = "wa9sF+y4HsXJ2IqRQcTadD32XYH7lG01BLuw9O9AbkTSbdRUvC4CU6vOvAKCE4LGU0AgIBSwSyumjqfA22ZZVWQxrkmbxfDaupCQ3tPD0yrY67su+hl6Iw1oKWVpWo3JWOg7RFFphGSz3x5MY/aqMgdB04t89/1O/w1cDnyilFU=";
-$response = set_richmenu_default($richMenuId,$ACCESS_TOKEN);
+
 file_put_contents("php://stderr", "POST JSON ===> ".$response);
 
 function set_richmenu_default($richMenuId,$ACCESS_TOKEN)
