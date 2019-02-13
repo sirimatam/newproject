7abc7572d47f878ae79fe607e48d1f09e759e719
sirimatam <45136358+sirimatam@users.noreply.github.com>
2019-02-13 16:37:12 +0700

    Update and rename setrichMenu1Default.php to setrichMenuDefault.php

--- RichMenu/setrichMenuDefault.php
+++ RichMenu/setrichMenuDefault.php
@@ -1,6 +1,9 @@
 <?php
+/*
+$richMenuId1 = "richmenu-ff58dd0a3a6e5f68cfc40afae5abe6ad"; //page1
+$richMenuId2= "richmenu-717a8ebccd0d4a7e0ca2c85d77a50f10"; //page2
+*/
 
-$richMenuId = "richmenu-ff58dd0a3a6e5f68cfc40afae5abe6ad"; //page1
 
 $ACCESS_TOKEN = 'wa9sF+y4HsXJ2IqRQcTadD32XYH7lG01BLuw9O9AbkTSbdRUvC4CU6vOvAKCE4LGU0AgIBSwSyumjqfA22ZZVWQxrkmbxfDaupCQ3tPD0yrY67su+hl6Iw1oKWVpWo3JWOg7RFFphGSz3x5MY/aqMgdB04t89/1O/w1cDnyilFU=';
 
