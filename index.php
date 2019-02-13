c1f528b7bab8f13d76d78f37749896c85db909a0
sirimatam <45136358+sirimatam@users.noreply.github.com>
2019-02-13 15:49:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -329,8 +329,35 @@ if ( sizeof($request_array['events']) > 0 )
 
 */
 
+$RICH_URL = 'https://api.line.me/v2/bot/richmenu';
+$rich_area1 = array(
+		  array('bounds'=> array( 'x'=>'0','y'=>'10','width' => 833,'height' => 833 ), 'action' => array('type'=> 'message', 'text' =>'ค้นหาสินค้า')),
+		  array('bounds'=> array( 'x'=>'843','y'=>'9','width' => 814,'height' => 824 ), 'action' => array('type'=> 'message', 'text' =>'โปรโมชั่น')),
+		  array('bounds'=> array( 'x'=>'1677','y'=>'0','width' => 823,'height' => 843 ), 'action' => array('type'=> 'message', 'text' =>'ตะกร้าของฉัน')),
+		  array('bounds'=> array( 'x'=>'0','y'=>'853','width' => 824,'height' => 833 ), 'action' => array('type'=> 'message', 'text' =>'ชำระเงิน')),
+      		  array('bounds'=> array( 'x'=>'843','y'=>'843','width' => 814,'height' => 843 ), 'action' => array('type'=> 'message', 'text' =>'เกี่ยวกับร้านค้า')),
+		  array('bounds'=> array( 'x'=>'1667','y'=>'843','width' => 833,'height' => 843 ), 'action' => array('type'=> 'message', 'text' =>'หน้าถัดไป'))
+		  );
+$rich_object1 = array('size'=> array('width'=>2500,'height'=>1686),'selected'=> true ,
+			     'name'=>'rich_menu','chatBarText'=>'Menu','areas'=>  $rich_area1 );
 
+$data1 = send_reply_message($RICH_URL, $POST_HEADER, $rich_object1);
+file_put_contents("php://stderr", "ID 1 =====> ".$data1);
 
+$rich_area2 = array(
+		  array('bounds'=> array( 'x'=>'0','y'=>'10','width' => 833,'height' => 833 ), 'action' => array('type'=> 'message', 'text' =>'ที่รอชำระเงิน')),
+		  array('bounds'=> array( 'x'=>'843','y'=>'9','width' => 814,'height' => 824 ), 'action' => array('type'=> 'message', 'text' =>'ที่ต้องจัดส่ง')),
+		  array('bounds'=> array( 'x'=>'1677','y'=>'0','width' => 823,'height' => 843 ), 'action' => array('type'=> 'message', 'text' =>'ที่ต้องได้รับ')),
+		  array('bounds'=> array( 'x'=>'0','y'=>'853','width' => 824,'height' => 833 ), 'action' => array('type'=> 'message', 'text' =>'สินค้าที่ถูกใจ')),
+      		  array('bounds'=> array( 'x'=>'843','y'=>'843','width' => 814,'height' => 843 ), 'action' => array('type'=> 'message', 'text' =>'ที่อยู่จัดส่ง')),
+		  array('bounds'=> array( 'x'=>'1667','y'=>'843','width' => 833,'height' => 843 ), 'action' => array('type'=> 'message', 'text' =>'กลับหน้าแรก'))
+		  );
+$rich_object2 = array('size'=> array('width'=>2500,'height'=>1686),'selected'=> false ,
+			     'name'=>'rich_menu','chatBarText'=>'Menu','areas'=>  $rich_area2 );
+
+$data2 = send_reply_message($RICH_URL, $POST_HEADER, $rich_object2);
+file_put_contents("php://stderr", "ID 2 =====> ".$data2);
+	
 
 function format_message($reply_token,$message)
 {
