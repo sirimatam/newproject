6fcb5df85895b1b55c316526c2e721f75a8c5cd0
sirimatam <45136358+sirimatam@users.noreply.github.com>
2019-02-13 17:57:49 +0700

    insert unlink richmenu

--- RichMenu/uploadandsetrichMenuDefault.php
+++ RichMenu/uploadandsetrichMenuDefault.php
@@ -88,5 +88,34 @@ function set_richmenu_default($richMenuId,$ACCESS_TOKEN)
 	    }	
 }	
 
+function unlink_richmenu($userid,$ACCESS_TOKEN)
+{
+	$curl = curl_init();
+    	curl_setopt_array($curl, array(
+      CURLOPT_URL => "https://api.line.me/v2/bot/user/".$userid."/richmenu",
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "DELETE",
+      CURLOPT_POSTFIELDS => $post_data,
+      CURLOPT_HTTPHEADER => array(
+        "authorization: Bearer ".$ACCESS_TOKEN,
+        "cache-control: no-cache",
+        "content-type: application/json; charset=UTF-8",
+      ),
+    ));
+	
+	 $result = curl_exec($curl);
+	 $err = curl_error($curl);
+	 curl_close($curl);
+	 if ($err) {
+		return $err;
+	    } else {
+		return $result;
+	    }	
+}	
+
 
 ?>
