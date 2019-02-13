42d1b3332be3dbb346f4986760929c07d0476fc9
sirimatam <45136358+sirimatam@users.noreply.github.com>
2019-02-13 15:40:53 +0700

    Create setrichMenu2Default.php

---
+++ RichMenu/setrichMenu2Default.php
@@ -0,0 +1,43 @@
+<?php
+
+
+$richMenuId = "richmenu-e05a96a7dd92be7ab66c7370733c5d20"; //page2
+$ACCESS_TOKEN = 'wa9sF+y4HsXJ2IqRQcTadD32XYH7lG01BLuw9O9AbkTSbdRUvC4CU6vOvAKCE4LGU0AgIBSwSyumjqfA22ZZVWQxrkmbxfDaupCQ3tPD0yrY67su+hl6Iw1oKWVpWo3JWOg7RFFphGSz3x5MY/aqMgdB04t89/1O/w1cDnyilFU=';
+
+//set rich menu default after upload img 
+$ACCESS_TOKEN = "wa9sF+y4HsXJ2IqRQcTadD32XYH7lG01BLuw9O9AbkTSbdRUvC4CU6vOvAKCE4LGU0AgIBSwSyumjqfA22ZZVWQxrkmbxfDaupCQ3tPD0yrY67su+hl6Iw1oKWVpWo3JWOg7RFFphGSz3x5MY/aqMgdB04t89/1O/w1cDnyilFU=";
+$response = set_richmenu_default($richMenuId,$ACCESS_TOKEN);
+file_put_contents("php://stderr", "POST JSON ===> ".$response);
+
+function set_richmenu_default($richMenuId,$ACCESS_TOKEN)
+{
+	$curl = curl_init();
+    	curl_setopt_array($curl, array(
+      CURLOPT_URL => "https://api.line.me/v2/bot/user/all/richmenu/".$richMenuId,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "POST",
+      CURLOPT_POSTFIELDS => $post_data,
+      CURLOPT_HTTPHEADER => array(
+        "authorization: Bearer ".$ACCESS_TOKEN,
+        "cache-control: no-cache",
+        "content-type: application/json; charset=UTF-8",
+      ),
+    ));
+	
+	 $result = curl_exec($curl);
+	 $err = curl_error($curl);
+	 curl_close($curl);
+	 if ($err) {
+		return $err;
+	    } else {
+		return $result;
+	    }	
+}	
+
+
+
+?>
