bd0da7eea28e811d0b96094fd4ddd73adf23090a
sirimatam <45136358+sirimatam@users.noreply.github.com>
2019-02-13 19:41:32 +0700

    Update uploadandsetrichMenuDefault.php

--- RichMenu/uploadandsetrichMenuDefault.php
+++ RichMenu/uploadandsetrichMenuDefault.php
@@ -2,7 +2,7 @@
 
 $richMenuId1 = "richmenu-ff58dd0a3a6e5f68cfc40afae5abe6ad";  // page1
 $richMenuId2 = "richmenu-717a8ebccd0d4a7e0ca2c85d77a50f10"; //page2
-
+/*
 $file1 = fopen('image/firstpage.png', 'r');
 $size1 = filesize('image/firstpage.png');
 
@@ -18,7 +18,7 @@ $size2 = filesize('image/secondpage.png');
 $fildata2 = fread($file2,$size2);
 $upload_pic2 = upload_richmenu($richMenuId2,$ACCESS_TOKEN,$fildata2,$file2);
 file_put_contents("php://stderr", "POST JSON2 ===> ".$upload_pic2);
-	
+*/	
 
 
 function upload_richmenu($richMenuId,$ACCESS_TOKEN,$fildata,$file)
