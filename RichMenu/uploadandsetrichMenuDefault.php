117eb3b9b1e907ad276e9987230d9007f0d5b389
sirimatam <45136358+sirimatam@users.noreply.github.com>
2019-02-13 16:45:37 +0700

    upload and setrichMenuDefault function

--- RichMenu/uploadandsetrichMenuDefault.php
+++ RichMenu/uploadandsetrichMenuDefault.php
@@ -56,4 +56,37 @@ $curl = curl_init();
 }	 
 
 
+//set rich menu default after upload img 
+
+
+function set_richmenu_default($richMenuId,$ACCESS_TOKEN)
+{
+	$curl = curl_init();
+    	curl_setopt_array($curl, array(
+      CURLOPT_URL => "https://api.line.me/v2/bot/user/all/richmenu/".$richMenuId,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "POST",
+      CURLOPT_POSTFIELDS => $post_data,
+      CURLOPT_HTTPHEADER => array(
+        "authorization: Bearer ".$ACCESS_TOKEN,
+        "cache-control: no-cache",
+        "content-type: application/json; charset=UTF-8",
+      ),
+    ));
+	
+	 $result = curl_exec($curl);
+	 $err = curl_error($curl);
+	 curl_close($curl);
+	 if ($err) {
+		return $err;
+	    } else {
+		return $result;
+	    }	
+}	
+
+
 ?>
