67df3ad25ffc8211e3bb33ac9a6b0caa89f668a0
sirimatam <45136358+sirimatam@users.noreply.github.com>
2019-02-13 16:27:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -329,30 +329,26 @@ if ( sizeof($request_array['events']) > 0 )
 
 */
 
-$richMenuId = "richmenu-ff58dd0a3a6e5f68cfc40afae5abe6ad";  // page1
-//$richMenuId2 = "richmenu-717a8ebccd0d4a7e0ca2c85d77a50f10"; //page2
+$richMenuId1 = "richmenu-ff58dd0a3a6e5f68cfc40afae5abe6ad";  // page1
+$richMenuId2 = "richmenu-717a8ebccd0d4a7e0ca2c85d77a50f10"; //page2
+
+$file1 = fopen('image/firstpage.png', 'r');
+$size1 = filesize('image/firstpage.png');
 
 
+$fildata1 = fread($file1,$size1);
+$upload_pic1 = upload_richmenu($richMenuId1,$ACCESS_TOKEN,$fildata1,$file1);
+file_put_contents("php://stderr", "POST JSON1 ===> ".$upload_pic1);
 
-$file = fopen('/image/firstpage.png', 'r');
-$size = filesize('/image/firstpage.png');
-
-
-$fildata = fread($file,$size);
-$upload_pic = upload_richmenu($richMenuId,$ACCESS_TOKEN,$fildata,$file);
-file_put_contents("php://stderr", "POST JSON1 ===> ".$upload_pic);
-
-/*
-
-$file2 = fopen('/image/secondpage.png', 'r');
-$size2 = filesize('/image/secondpage.png');
+$file2 = fopen('image/secondpage.png', 'r');
+$size2 = filesize('image/secondpage.png');
 
 
 $fildata2 = fread($file2,$size2);
 $upload_pic2 = upload_richmenu($richMenuId2,$ACCESS_TOKEN,$fildata2,$file2);
 file_put_contents("php://stderr", "POST JSON2 ===> ".$upload_pic2);
 	
-*/
+
 
 function upload_richmenu($richMenuId,$ACCESS_TOKEN,$fildata,$file)
 {
