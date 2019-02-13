9a47f6899bc663242525d8a1bd9a16182edebdea
sirimatam <45136358+sirimatam@users.noreply.github.com>
2019-02-13 16:27:06 +0700

    Update uploadrichMenu.php

--- RichMenu/uploadrichMenu.php
+++ RichMenu/uploadrichMenu.php
@@ -3,24 +3,16 @@
 $richMenuId1 = "richmenu-ff58dd0a3a6e5f68cfc40afae5abe6ad";  // page1
 $richMenuId2 = "richmenu-717a8ebccd0d4a7e0ca2c85d77a50f10"; //page2
 
-$rich_img_url1 = 'https://api.line.me/v2/bot/richmenu/'.$richMenuId1.'/content';
-
-
-$file1 = fopen('/image/firstpage.png', 'r');
-$size1 = filesize('/image/firstpage.png');
+$file1 = fopen('image/firstpage.png', 'r');
+$size1 = filesize('image/firstpage.png');
 
 
 $fildata1 = fread($file1,$size1);
 $upload_pic1 = upload_richmenu($richMenuId1,$ACCESS_TOKEN,$fildata1,$file1);
 file_put_contents("php://stderr", "POST JSON1 ===> ".$upload_pic1);
 
-
-
-$rich_img_url2 = 'https://api.line.me/v2/bot/richmenu/'.$richMenuId2.'/content';
-
-
-$file2 = fopen('/image/secondpage.png', 'r');
-$size2 = filesize('/image/secondpage.png');
+$file2 = fopen('image/secondpage.png', 'r');
+$size2 = filesize('image/secondpage.png');
 
 
 $fildata2 = fread($file2,$size2);
