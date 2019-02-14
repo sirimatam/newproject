d0ebfe1c4eaae9831b90da9827aa8bb08c3c9175
sirimatam <45136358+sirimatam@users.noreply.github.com>
2019-02-14 14:32:13 +0700

    Update user_function.php

--- user_function.php
+++ user_function.php
@@ -131,22 +131,22 @@ function carousel_flex_order($db,$userid,$check)
 	{
 		if($check=='1') // ที่รอชำระเงิน
 		{
-			$a = pg_query($db,"SELECT cartp_id FROM orderlist WHERE cartp_id = '$cartp_id' AND order_status == 'waiting for payment'");
-			$b = pg_query($db,"SELECT total_price FROM orderlist WHERE cartp_id = '$cartp_id' AND order_status == 'waiting for payment'");
-			$c = pg_query($db,"SELECT order_id FROM orderlist WHERE cartp_id = '$cartp_id' AND order_status == 'waiting for payment'");
+			$a = pg_query($db,"SELECT cartp_id FROM orderlist WHERE cartp_id = '$cartp_id' AND order_status = 'waiting for payment'");
+			$b = pg_query($db,"SELECT total_price FROM orderlist WHERE cartp_id = '$cartp_id' AND order_status = 'waiting for payment'");
+			$c = pg_query($db,"SELECT order_id FROM orderlist WHERE cartp_id = '$cartp_id' AND order_status = 'waiting for payment'");
 		}
 		elseif($check=='2')//ที่ต้องจัดส่ง
 		{
-			$a = pg_query($db,"SELECT cartp_id FROM orderlist WHERE cartp_id = '$cartp_id' AND  order_status == 'waiting for packing'");
-			$b = pg_query($db,"SELECT total_price FROM orderlist WHERE cartp_id = '$cartp_id' AND  order_status == 'waiting for packing'");
-			$c = pg_query($db,"SELECT order_id FROM orderlist WHERE cartp_id = '$cartp_id' AND order_status == 'waiting for packing'");
+			$a = pg_query($db,"SELECT cartp_id FROM orderlist WHERE cartp_id = '$cartp_id' AND  order_status = 'waiting for packing'");
+			$b = pg_query($db,"SELECT total_price FROM orderlist WHERE cartp_id = '$cartp_id' AND  order_status = 'waiting for packing'");
+			$c = pg_query($db,"SELECT order_id FROM orderlist WHERE cartp_id = '$cartp_id' AND order_status = 'waiting for packing'");
 			
 		}
 		elseif(strlen($check)>1)//ที่ต้องได้รับ
 		{
-			$a = pg_query($db,"SELECT cartp_id FROM orderlist WHERE cartp_id = '$cartp_id' AND order_status!= 'waiting for payment' AND order_status != 'waiting for packing'");
-			$b = pg_query($db,"SELECT total_price FROM orderlist WHERE cartp_id = '$cartp_id' AND order_status!= 'waiting for payment' AND order_status != 'waiting for packing'");
-			$c = pg_query($db,"SELECT order_id FROM orderlist WHERE cartp_id = '$cartp_id' AND order_status!= 'waiting for payment' AND order_status != 'waiting for packing'");
+			$a = pg_query($db,"SELECT cartp_id FROM orderlist WHERE cartp_id = '$cartp_id' AND order_status != 'waiting for payment' AND order_status != 'waiting for packing'");
+			$b = pg_query($db,"SELECT total_price FROM orderlist WHERE cartp_id = '$cartp_id' AND order_status != 'waiting for payment' AND order_status != 'waiting for packing'");
+			$c = pg_query($db,"SELECT order_id FROM orderlist WHERE cartp_id = '$cartp_id' AND order_status != 'waiting for payment' AND order_status != 'waiting for packing'");
 		}
 				
 		if(pg_num_rows($a)>0)
