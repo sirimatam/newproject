4af1fce41df214ba81e7e3e28d7b3a492a7499fa
ssirawan <45140043+ssirawan@users.noreply.github.com>
2019-03-10 15:09:43 +0700

    Update store_function.php

--- store_function.php
+++ store_function.php
@@ -892,7 +892,7 @@ function flex_sku($db,$skuid)
 	$prod_id = $sku[1];
 	$prod = pg_fetch_row(pg_query($db,"SELECT * FROM product WHERE prod_id = '$prod_id' "))[0];
 	
-	file_put_contents("php://stderr", "prod id =====> ".$prod_id);
+	file_put_contents("php://stderr", "prod id =====> ".$sku[1]);
 	file_put_contents("php://stderr", "prod name =====> ".$prod[1]);
 	
 	$datas['type'] = 'flex';
