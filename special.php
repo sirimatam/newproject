d26673a3b837ae5ef7c10b806c64c55ffaeaa9c9
ssirawan <45140043+ssirawan@users.noreply.github.com>
2019-03-12 10:55:40 +0700

    Create special.php

---
+++ special.php
@@ -0,0 +1,27 @@
+<?php
+
+function quickreply()
+{
+  $data = array();
+  $data['quickReply']['items'][0]['type'] = 'action';
+  $data['quickReply']['items'][0]['action']['type'] = 'camera';
+  $data['quickReply']['items'][0]['action']['label'] = 'Open camera';
+  $data['quickReply']['items'][1]['type'] = 'action';
+  $data['quickReply']['items'][1]['action']['type'] = 'cameraRoll';
+  $data['quickReply']['items'][1]['action']['label'] = 'Send Photo';
+  $data['quickReply']['items'][2]['action']['type'] = 'location';
+  $data['quickReply']['items'][2]['action']['label'] = 'Location';
+  $data['quickReply']['items'][3]['action']['type'] = 'datetimepicker';
+  $data['quickReply']['items'][3]['action']['label'] = 'Select date and time';
+  $data['quickReply']['items'][3]['action']['data'] = '12345';
+  $data['quickReply']['items'][3]['action']['mode'] = 'datetime';
+  
+  return $data;
+}
+
+
+
+
+
+
+?>
