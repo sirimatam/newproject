a5b64110e2375101dcc49fa2475c9f1464859e72
ssirawan <45140043+ssirawan@users.noreply.github.com>
2019-03-12 11:03:23 +0700

    Update special.php

--- special.php
+++ special.php
@@ -3,18 +3,20 @@
 function quickreply()
 {
   $data = array();
-  $data['quickReply']['items'][0]['type'] = 'action';
-  $data['quickReply']['items'][0]['action']['type'] = 'camera';
-  $data['quickReply']['items'][0]['action']['label'] = 'Open camera';
-  $data['quickReply']['items'][1]['type'] = 'action';
-  $data['quickReply']['items'][1]['action']['type'] = 'cameraRoll';
-  $data['quickReply']['items'][1]['action']['label'] = 'Send Photo';
-  $data['quickReply']['items'][2]['action']['type'] = 'location';
-  $data['quickReply']['items'][2]['action']['label'] = 'Location';
-  $data['quickReply']['items'][3]['action']['type'] = 'datetimepicker';
-  $data['quickReply']['items'][3]['action']['label'] = 'Select date and time';
-  $data['quickReply']['items'][3]['action']['data'] = '12345';
-  $data['quickReply']['items'][3]['action']['mode'] = 'datetime';
+  $data['messages']['type'] = 'text';
+	$data['messages']['text'] = 'Hello Quick Reply!';
+  $data['messages']['quickReply']['items'][0]['type'] = 'action';
+  $data['messages']['quickReply']['items'][0]['action']['type'] = 'camera';
+  $data['messages']['quickReply']['items'][0]['action']['label'] = 'Open camera';
+  $data['messages']['quickReply']['items'][1]['type'] = 'action';
+  $data['messages']['quickReply']['items'][1]['action']['type'] = 'cameraRoll';
+  $data['messages']['quickReply']['items'][1]['action']['label'] = 'Send Photo';
+  $data['messages']['quickReply']['items'][2]['action']['type'] = 'location';
+  $data['messages']['quickReply']['items'][2]['action']['label'] = 'Location';
+  $data['messages']['quickReply']['items'][3]['action']['type'] = 'datetimepicker';
+  $data['messages']['quickReply']['items'][3]['action']['label'] = 'Select date and time';
+  $data['messages']['quickReply']['items'][3]['action']['data'] = '12345';
+  $data['messages']['quickReply']['items'][3]['action']['mode'] = 'datetime';
   
   return $data;
 }
