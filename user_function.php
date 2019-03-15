539fead1ae9eda1c2b2f11baa7dfe5847e7fa2a8
ssirawan <45140043+ssirawan@users.noreply.github.com>
2019-03-15 11:48:59 +0700

    Update user_function.php

--- user_function.php
+++ user_function.php
@@ -596,5 +596,36 @@ function timepost()
 	return ['type'=>'text','text' => [$datee,$timee] ];
 }
 
+function get_user_img($POST_HEADER,$imgid,$orderid)
+	{
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+		  CURLOPT_URL => "https://api.line.me/v2/bot/message/".$imgid."/content",
+		  CURLOPT_RETURNTRANSFER => true,
+		  CURLOPT_ENCODING => "",
+		  CURLOPT_MAXREDIRS => 10,
+		  CURLOPT_TIMEOUT => 30,
+		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		  CURLOPT_CUSTOMREQUEST => "GET",
+		  CURLOPT_POSTFIELDS => "",
+		  CURLOPT_HTTPHEADER => $POST_HEADER,
+		));
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+	
+		curl_close($curl);
+		if ($err) {
+		  	echo "cURL Error #:" . $err;
+		} else {
+		  	define('UPLOAD_DIR', 'image/');
+		  	$img=base64_encode($response);
+			$data = base64_decode($img);
+			$file = UPLOAD_DIR . $orderid . '.jpg';
+			$success = file_put_contents($file, $data);
+		}
+	}
+
+
+
    
 ?>
