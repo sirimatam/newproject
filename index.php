28fd240ca431d517a038fb59ed27faff7113157c
sirimatam <45136358+sirimatam@users.noreply.github.com>
2019-03-15 15:31:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -300,7 +300,7 @@ if ( sizeof($request_array['events']) > 0 )
 	   	   
 	   $success = file_put_contents($file, $data);	   
 	   
-	   file_put_contents("php://stderr", "image 64  ===> ".json_encode($data));
+	   file_put_contents("php://stderr", "image 64  ===> ".$response);
 	   
 	   $datetime = get_datetime();
 	   
