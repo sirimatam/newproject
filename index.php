0f729ce31debaca8c2f3d7634ce1195ef413647b
sirimatam <45136358+sirimatam@users.noreply.github.com>
2019-03-25 10:55:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include ('special.php');
 
 
 $richMenuId1 = "richmenu-ff58dd0a3a6e5f68cfc40afae5abe6ad"; //page1
-$richMenuId2= "richmenu-b5605d39250019a4ad9734dffc7d23ef"; //page2
+$richMenuId2= "richmenu-cc0063317dd8d062a9be46ef81a8e717"; //page2 new! 25/03
 
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
@@ -15,7 +15,7 @@ $API_URL_push = 'https://api.line.me/v2/bot/message/push';
 $ACCESS_TOKEN = 'wa9sF+y4HsXJ2IqRQcTadD32XYH7lG01BLuw9O9AbkTSbdRUvC4CU6vOvAKCE4LGU0AgIBSwSyumjqfA22ZZVWQxrkmbxfDaupCQ3tPD0yrY67su+hl6Iw1oKWVpWo3JWOg7RFFphGSz3x5MY/aqMgdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
-set_richmenu_default($richMenuId1,$ACCESS_TOKEN);
+set_richmenu_default($richMenuId2,$ACCESS_TOKEN);
 
 
 
