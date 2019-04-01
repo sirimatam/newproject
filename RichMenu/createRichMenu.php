89829da00bbddf4aac0ec834a2d4414e0ea9730c
ssirawan <45140043+ssirawan@users.noreply.github.com>
2019-04-01 12:16:50 +0700

    Add files via upload

--- RichMenu/createRichMenu.php
+++ RichMenu/createRichMenu.php
@@ -2,7 +2,8 @@
 
 $ACCESS_TOKEN = 'wa9sF+y4HsXJ2IqRQcTadD32XYH7lG01BLuw9O9AbkTSbdRUvC4CU6vOvAKCE4LGU0AgIBSwSyumjqfA22ZZVWQxrkmbxfDaupCQ3tPD0yrY67su+hl6Iw1oKWVpWo3JWOg7RFFphGSz3x5MY/aqMgdB04t89/1O/w1cDnyilFU=';
 
-$RICH_URL = 'https://api.line.me/v2/bot/richmenu';
+$RICH_URL = 'https://api.line.me/v2/bot/richmenu';  
+/*
 $rich_area1 = array(
 		  array('bounds'=> array( 'x'=>'0','y'=>'10','width' => 833,'height' => 833 ), 'action' => array('type'=> 'message', 'text' =>'ค้นหาสินค้า')),
 		  array('bounds'=> array( 'x'=>'843','y'=>'9','width' => 814,'height' => 824 ), 'action' => array('type'=> 'message', 'text' =>'โปรโมชั่น')),
@@ -16,7 +17,7 @@ $rich_object1 = array('size'=> array('width'=>2500,'height'=>1686),'selected'=> 
 
 $data1 = send_reply_message($RICH_URL, $POST_HEADER, $rich_object1);
 file_put_contents("php://stderr", "ID 1 =====> ".$data1);
-
+*/
 $rich_area2 = array(
 		  array('bounds'=> array( 'x'=>'0','y'=>'10','width' => 833,'height' => 833 ), 'action' => array('type'=> 'message', 'text' =>'ที่ต้องจัดส่ง')),
 		  array('bounds'=> array( 'x'=>'843','y'=>'9','width' => 814,'height' => 824 ), 'action' => array('type'=> 'message', 'text' =>'ที่ต้องได้รับ')),
--- RichMenu/uploadandsetrichMenuDefault.php
+++ RichMenu/uploadandsetrichMenuDefault.php
@@ -1,8 +1,9 @@
 <?php
 
+
 $richMenuId1 = "richmenu-ff58dd0a3a6e5f68cfc40afae5abe6ad";  // page1
-$richMenuId2 = "richmenu-b5605d39250019a4ad9734dffc7d23ef"; //page2 old
-$richMenuId2 = "richmenu-cc0063317dd8d062a9be46ef81a8e717"; // new
+$richMenuId3= "richmenu-b5605d39250019a4ad9734dffc7d23ef"; //page2 old
+$richMenuId2 = "richmenu-cc0063317dd8d062a9be46ef81a8e717"; //page2 new
 /*
 $file1 = fopen('image/firstpage.png', 'r');
 $size1 = filesize('image/firstpage.png');
@@ -12,16 +13,16 @@ $fildata1 = fread($file1,$size1);
 $upload_pic1 = upload_richmenu($richMenuId1,$ACCESS_TOKEN,$fildata1,$file1);
 file_put_contents("php://stderr", "POST JSON1 ===> ".$upload_pic1);
 
-$file2 = fopen('image/secondpage2.png', 'r');
-$size2 = filesize('image/secondpage2.png');
+$file2 = fopen('C://xampp/htdocs/image/secondpage2.png', 'r');
+$size2 = filesize('C://xampp/htdocs/image/secondpage2.png');
 
 
 $fildata2 = fread($file2,$size2);
 $upload_pic2 = upload_richmenu($richMenuId2,$ACCESS_TOKEN,$fildata2,$file2);
-file_put_contents("php://stderr", "POST JSON2 ===> ".$upload_pic2);
-*/	
+file_put_contents("php://stderr", "upload pic2 success ===> ".$upload_pic2);
 
 
+*/
 function upload_richmenu($richMenuId,$ACCESS_TOKEN,$fildata,$file)
 {
 $curl = curl_init();
@@ -58,7 +59,7 @@ $curl = curl_init();
 
 
 //set rich menu default after upload img 
-
+$post_data = Array();
 
 function set_richmenu_default($richMenuId,$ACCESS_TOKEN)
 {
@@ -71,9 +72,8 @@ function set_richmenu_default($richMenuId,$ACCESS_TOKEN)
       CURLOPT_TIMEOUT => 30,
       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
       CURLOPT_CUSTOMREQUEST => "POST",
-      //CURLOPT_POSTFIELDS => $post_data,
-      CURLOPT_POST => 1,
-	CURLOPT_HTTPHEADER => array(
+      CURLOPT_POSTFIELDS => $post_data,
+      CURLOPT_HTTPHEADER => array(
         "authorization: Bearer ".$ACCESS_TOKEN,
         "cache-control: no-cache",
         "content-type: application/json; charset=UTF-8",
@@ -102,6 +102,7 @@ function unlink_richmenu($userid,$ACCESS_TOKEN)
       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
       CURLOPT_CUSTOMREQUEST => "DELETE",
       CURLOPT_POSTFIELDS => $post_data,
+	  //CURLOPT_POST => 1,
       CURLOPT_HTTPHEADER => array(
         "authorization: Bearer ".$ACCESS_TOKEN,
         "cache-control: no-cache",
